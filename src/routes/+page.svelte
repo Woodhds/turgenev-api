f322dab5c3bd34b99971367021f689a6c35fc29a
borodin_s <borodin_s@tochka.com>
2025-09-06 03:29:22 +0300

    add some improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
     import { foo } from "./data.remote";
     import { onMount } from "svelte";
+    import { flip } from "svelte/animate";
 
     type UserState = {
         token: string;
         history: { link: string; date: string }[];
         lastText: string;
     };
-    let loading = $state(false);
+    let loading = $state(true);
+    let api = $state("risk");
 
     let userState = $state<UserState>({
         token: "",
@@ -15,12 +17,21 @@
         lastText: "",
     });
     let text = $state(userState?.lastText);
+    let history = $derived.by(() => {
+        const s = userState.history.map((z) => ({
+            date: new Date(z.date),
+            link: z.link,
+        }));
+        s.sort((a, b) => b.date - a.date);
+        return s;
+    });
 
     onMount(() => {
         const localState = localStorage.getItem("user_state");
         if (localState) {
             userState = JSON.parse(localState) as UserState;
         }
+        loading = false;
     });
 
     const onSubmit = async ({ submit }) => {
@@ -40,6 +51,10 @@
         } finally {
             loading = false;
         }
+    };
+
+    const isSelected = (is: string) => {
+        return is === api;
     };
 
     const removeToken = () => {
@@ -65,7 +80,7 @@
 
 <div class="grid grid-cols-2 gap-x-16 w-full">
     {#if loading}
-        <div class="skeleton h-[32rem] w-[32rem]"></div>
+        <div class="skeleton h-[40rem] w-[44rem]"></div>
     {:else if !userState.token}
         <form onsubmit={addToken} class="flex flex-col w-1/2">
             <label for="token" class="label mb-4">Добавить ключ</label>
@@ -80,9 +95,27 @@
                 >
             </div>
             <form {...foo.enhance(onSubmit)} class="grid grid-cols-1 gap-y-5">
-                <select class="select" name="api">
-                    <option>risk</option>
-                    <option>frequency</option>
+                <select class="select" name="api" bind:value={api}>
+                    <option value="risk" selected={isSelected("risk")}
+                        >Общий риск</option
+                    >
+                    <option value="frequency" selected={isSelected("frequency")}
+                        >Повторы</option
+                    >
+                    <option value="style" selected={isSelected("style")}
+                        >Стилистика</option
+                    >
+                    <option value="keywords" selected={isSelected("keywords")}
+                        >Запросы</option
+                    >
+                    <option value="formality" selected={isSelected("formality")}
+                        >Водность</option
+                    >
+                    <option
+                        value="readability"
+                        selected={api === "readability" ? true : false}
+                        >Удобочитаемость</option
+                    >
                 </select>
                 <input type="hidden" name="key" value={userState.token} />
 
@@ -102,15 +135,21 @@
         </div>
     {/if}
 
-    <div class="w-1/2 mt-12">
-        {#each userState.history as { link, date }}
-            <a
-                class="mb-4 link w-full block"
-                target="_blank"
-                href={"https://turgenev.ashmanov.com/?t=" + link}
-                >ссылка - {date}
-            </a>
-        {/each}
+    <div class="w-1/2 mt-4">
+        <h2 class="text-2xl mb-6">Последние 10 запросов</h2>
+        <ol>
+            {#each history as { link, date } (link + date)}
+                <li animate:flip>
+                    <a
+                        id={link}
+                        class="mb-4 link w-full block"
+                        target="_blank"
+                        href={"https://turgenev.ashmanov.com/?t=" + link}
+                        >ссылка - {date.toLocaleString()}
+                    </a>
+                </li>
+            {/each}
+        </ol>
     </div>
 </div>
 
